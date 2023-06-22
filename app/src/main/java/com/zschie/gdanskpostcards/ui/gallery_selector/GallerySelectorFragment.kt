fbc60aa37e635fbffb963c5aa1d580564b733bc5
mmorawski520 <badylcookiestyle@gmail.com>
2023-06-22 08:54:10 +0000

    xd <3

--- app/src/main/java/com/zschie/gdanskpostcards/ui/gallery_selector/GallerySelectorFragment.kt
+++ app/src/main/java/com/zschie/gdanskpostcards/ui/gallery_selector/GallerySelectorFragment.kt
@@ -1,13 +1,12 @@
-package com.zschie.gdanskpostcards
+package com.zschie.gdanskpostcards.ui.gallery_selector
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
 import androidx.navigation.fragment.findNavController
+import com.zschie.gdanskpostcards.R
 import com.zschie.gdanskpostcards.databinding.FragmentGallerySelectorBinding
 
 class GallerySelectorFragment : Fragment() {
