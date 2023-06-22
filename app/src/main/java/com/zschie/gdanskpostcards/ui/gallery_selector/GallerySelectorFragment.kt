ca476c67d9d339ca2961c7c97e6112fb7a6774e7
mmorawski520 <badylcookiestyle@gmail.com>
2023-06-22 08:47:12 +0000

    epic layout changes <3

--- app/src/main/java/com/zschie/gdanskpostcards/ui/gallery_selector/GallerySelectorFragment.kt
+++ app/src/main/java/com/zschie/gdanskpostcards/ui/gallery_selector/GallerySelectorFragment.kt
@@ -1,16 +1,13 @@
-package com.zschie.gdanskpostcards
+package com.zschie.gdanskpostcards.ui.gallery_selector
 
-import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
 import com.zschie.gdanskpostcards.databinding.FragmentGallerySelectorBinding
 
-class GallerySelector : Fragment() {
+class GallerySelectorFragment : Fragment() {
 
 
     private var _binding: FragmentGallerySelectorBinding? = null
